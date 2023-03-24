3c68a9576d5bac2b7b0d743c81f36dd7dcc5c27c
Unkx <mitie931@gmail.com>
2023-03-24 14:57:09 +0100

    zad.1

---
+++ Zad.1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj długość boku sześcianu : ");
+        int bok = int.Parse(Console.ReadLine());
+        int objetosc = SzescianObjetosc(bok);
+        int polePowierzchni = SzescianPolePowierzchni(bok);
+
+        Console.WriteLine("Objętość sześcianu : {0}", objetosc);
+        Console.WriteLine("Pole powierzchni sześcianu : {0}", polePowierzchni);
+
+    }
+    public static int SzescianObjetosc(int bok)
+        {
+            return bok * bok * bok;
+        }
+    public static int SzescianPolePowierzchni(int bok)
+    {
+        return 6 * bok * bok;
+    }
+}
+
