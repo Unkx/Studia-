8499e8d18b6c4f6ddfc9ada89c6c67df277a5d3d
Unkx <97525106+Unkx@users.noreply.github.com>
2023-05-20 20:07:28 +0100

    Delete script.js

--- HTML + JavaScript/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-  var nav = document.querySelector('.sticky-nav');
-  if (window.pageYOffset > nav.offsetTop) {
-    nav.classList.add('sticky');
-  } else {
-    nav.classList.remove('sticky');
-  }
-});
