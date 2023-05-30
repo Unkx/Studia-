ac96003af1e8b533bee35256c9e2114c18a53440
Unkx <mitie931@gmail.com>
2023-05-30 21:23:03 +0200

    /

---
+++ C#/Lista 9/BookForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Store;
+using static System.Windows.Forms.DataFormats;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WinFormsApp1
+{
+    public partial class BookForm : Form
+    {
+        public BookForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonUzupelnij_Click(object sender, EventArgs e)
+        {
+            txtImie.Text = "Jan";
+            txtNazwisko.Text = "Masło";
+            txtWiek.Text = "18";
+            txtAdres.Text = "Oleska, Opole";
+            txtKoszyk.Text = "20";
+
+            txtImieAutora.Text = "Andrzej";
+            txtNazwiskoAutora.Text = "Mleko";
+            txtTytul.Text = "Poradnik majstra";
+            txtRokWyd.Text = "2000";
+            txtPrzeczytaneStrony.Text = "69";
+
+
+            txtUlubionyAutor.Text = "Paweł X";
+            txtUlubionaKsiazka.Text = "Pod Zamkiem";
+            txtKupionaKsiazka.Text = "Harry";
+            txtRokWydania.Text = "2020";
+            txtPrzeczytaneKs.Text = "33";
+
+
+        }
+        private void ClearTextBoxes()
+        {
+            txtImie.Clear();
+            txtNazwisko.Clear();
+            txtWiek.Clear();
+            txtAdres.Clear();
+            txtKoszyk.Clear();
+
+            txtImieAutora.Clear();
+            txtNazwiskoAutora.Clear();
+            txtTytul.Clear();
+            txtRokWyd.Clear();
+            txtPrzeczytaneStrony.Clear();
+
+
+            txtUlubionyAutor.Clear();
+            txtUlubionaKsiazka.Clear();
+            txtKupionaKsiazka.Clear();
+            txtRokWydania.Clear();
+            txtPrzeczytaneKs.Clear();
+
+        }
+
+        private void buttonZaladuj_Click(object sender, System.EventArgs e)
+        {
+
+            Book s1 = new Book(txtImie.Text, txtNazwisko.Text, Convert.ToInt32(this.txtWiek.Text), txtAdres.Text, Convert.ToInt32(this.txtKoszyk.Text), txtUlubionyAutor.Text, txtUlubionaKsiazka.Text, txtKupionaKsiazka.Text, Convert.ToInt32(this.txtRokWydania.Text), Convert.ToInt32(this.txtPrzeczytaneStrony.Text), dateCzas.Value, txtImieAutora.Text, txtNazwiskoAutora.Text, txtTytul.Text, Convert.ToInt32(this.txtRokWyd.Text), Convert.ToInt32(this.txtPrzeczytaneKs.Text));
+
+
+            Okno.listP.Add(s1); //dodanie obiektu do listy
+            listBoxBook.Items.Clear(); //wyczyszczenie listBoxa
+            Okno.listP[Okno.listP.Count - 1].Wypisz(listBoxBook);
+            ClearTextBoxes(); //wyczyszczenie pól tekstowych i innych elementów
+
+            Lista.listBoxData2 = new List<string>();
+            foreach (var item in listBoxBook.Items)
+            {
+                Lista.listBoxData2.Add(item.ToString());
+            }
+
+            Lista form4 = new Lista();
+
+            pictureZdjecie2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureZdjecie2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
---
+++ C#/Lista 9/Lista.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static WinFormsApp1.Okno;
+
+namespace WinFormsApp1
+{
+    public partial class Lista : Form
+    {
+        public static List<string> listBoxData { get; set; }
+        public static List<string> listBoxData2 { get; set; }
+
+        private List<MyClass> objectList;
+        public Lista()
+        {
+            InitializeComponent();
+
+            objectList = new List<MyClass>();
+        }
+
+        private void buttonPoprzedni_Click(object sender, EventArgs e)
+        {
+            if (listBoxData != null)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(listBoxData.ToArray());
+
+                pictureZdjecie2.Visible = true;
+                pictureZdjecie2.SizeMode = PictureBoxSizeMode.Zoom;
+                //pictureZdjecie2.Image = new Bitmap("C:\\Users\\mi140\\OneDrive\\Pulpit\\Bez nazwy-2.jpg");
+                pictureZdjecie2.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else if (listBoxData == null)
+            {
+                MessageBox.Show("Nic tu nie ma ");
+            }
+
+            listBox1.Items.AddRange(listBoxData.ToArray());
+
+            pictureZdjecie2.SizeMode = PictureBoxSizeMode.Zoom;
+            //pictureZdjecie2.Image = new Bitmap("C:\\Users\\mi140\\OneDrive\\Pulpit\\Bez nazwy-2.jpg");
+            pictureZdjecie2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+        }
+
+
+        private void buttonNastepny_Click(object sender, EventArgs e)
+        {
+
+            if (listBoxData2 != null)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(listBoxData2.ToArray());
+                pictureZdjecie2.Visible = true;
+                pictureZdjecie2.SizeMode = PictureBoxSizeMode.Zoom;
+                //pictureZdjecie2.Image = new Bitmap("C:\\Users\\mi140\\OneDrive\\Pulpit\\Bez nazwy-1.jpg");
+                pictureZdjecie2.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else if (listBoxData2 == null)
+            {
+                MessageBox.Show("Nic nie ma ");
+            }
+
+            listBox1.Items.AddRange(listBoxData2.ToArray());
+
+            pictureZdjecie2.SizeMode = PictureBoxSizeMode.Zoom;
+            //pictureZdjecie2.Image = new Bitmap("C:\\Users\\mi140\\OneDrive\\Pulpit\\Bez nazwy-1.jpg");
+            pictureZdjecie2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+
+
+
+        //lista 8//
+
+        private void buttonZapisz_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            saveFileDialog.Title = "Save List to File";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (MyClass obj in objectList)
+                    {
+                        obj.Zapisz(writer);
+                    }
+                }
+
+                MessageBox.Show("List saved to file successfully!");
+            }
+        }
+
+        private void buttonWczytaj_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+            openFileDialog.Title = "Load List from File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    objectList.Clear();
+
+                    while (!reader.EndOfStream)
+                    {
+                        MyClass obj = new MyClass();
+                        obj.Wczytaj(reader);
+                        objectList.Add(obj);
+                    }
+                }
+
+                MessageBox.Show("List loaded from file successfully!");
+
+            }
+        }
+        public class MyClass
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public void Zapisz(StreamWriter writer)
+            {
+                writer.WriteLine(Id);
+                writer.WriteLine(Name);
+            }
+
+            public void Wczytaj(StreamReader reader)
+            {
+                Id = int.Parse(reader.ReadLine());
+                Name = reader.ReadLine();
+            }
+        }
+
+        private void buttonWyswietl_Click(object sender, EventArgs e)
+        {
+            string message = "Dane z listy:\n";
+            foreach (MyClass obj in objectList)
+            {
+                message += "ID: " + obj.Id + ", Name: " + obj.Name + "\n";
+            }
+
+            MessageBox.Show(message);
+        }
+
+
+    }
+}
